a304969cf906c412588805bfd61e4e61ef1743ae
Robert O'Rourke <robert@o-rourke.org>
2014-05-06 15:50:57 +0100

    Using $. namespace directly rather than with $.fn.
    $.fn equivalent is added at the end

--- fumanchu.js
+++ fumanchu.js
@@ -7,14 +7,14 @@
 (function($){
 
 	// slightly rubbish handlebars-esque templating thing
-	$.fn.fumanchu = function( template, context, fallback, args ) {
+	$.fumanchu = function( template, context, fallback, args ) {
 
 		var t = this;
 
 		// set defaults
-		$.fn.fumanchu.context = context || {};
-		$.fn.fumanchu.fallback = fallback || context;
-		$.fn.fumanchu.args = args || {};
+		$.fumanchu.context = context || {};
+		$.fumanchu.fallback = fallback || context;
+		$.fumanchu.args = args || {};
 
 		// avoid breaking catastrophically
 		if ( $.type( template ) === 'undefined' )
@@ -22,14 +22,14 @@
 
 		// if template is a function then run it
 		if ( $.type( template ) === 'function' )
-			template = template.apply( t, [ $.fn.fumanchu.context, $.fn.fumanchu.fallback, $.fn.fumanchu.args ] );
+			template = template.apply( t, [ $.fumanchu.context, $.fumanchu.fallback, $.fumanchu.args ] );
 
 		// if template is an object then look for a template & override context
 		if ( $.type( template ) === 'object' ) {
 
 			// regular object, check for template entry
 			if ( template.template ) {
-				$.fn.fumanchu.context = template;
+				$.fumanchu.context = template;
 				template = template.template;
 
 			// check if its a jquery selection & try to get content out
@@ -44,13 +44,13 @@
 		// replace any double bracketed strings or double underscored
 		return template.toString().replace( /(?:__|{{)([a-z0-9\.]+)(?:}}|__)/gi, function( match, p1, offset, s ) {
 			// get object property
-			var val = $.fn.fumanchu.getpath( p1, $.fn.fumanchu.context, $.fn.fumanchu.fallback ),
+			var val = $.fumanchu.getpath( p1, $.fumanchu.context, $.fumanchu.fallback ),
 				out = '',
-				tpl = $.fn.fumanchu.templates[ p1 ] || $( '[data-template="' + p1 + '"]' ).html();
+				tpl = $.fumanchu.templates[ p1 ] || $( '[data-template="' + p1 + '"]' ).html();
 
 			// store template if found
-			if ( ! $.fn.fumanchu.templates[ p1 ] && tpl )
-				$.fn.fumanchu.templates[ p1 ] = tpl;
+			if ( ! $.fumanchu.templates[ p1 ] && tpl )
+				$.fumanchu.templates[ p1 ] = tpl;
 
 			// array type
 			if ( $.type( val ) === 'array' ) {
@@ -58,33 +58,33 @@
 					if ( $.type( item ) === 'object' ) {
 						if ( ! item.template && tpl )
 							item.template = tpl;
-						$.fn.fumanchu.context = item;
+						$.fumanchu.context = item;
 					}
-					out += t.fumanchu( item, $.fn.fumanchu.context, $.fn.fumanchu.fallback, { list: val, index: i } );
+					out += t.fumanchu( item, $.fumanchu.context, $.fumanchu.fallback, { list: val, index: i } );
 				} );
 			// object type
 			} else if ( $.type( val ) === 'object' && val.template ) {
-				out = t.fumanchu( val.template, val, $.fn.fumanchu.fallback, args );
+				out = t.fumanchu( val.template, val, $.fumanchu.fallback, args );
 			// function type
 			} else if ( $.type( val ) === 'function' ) {
-				out = val.apply( t, [ $.fn.fumanchu.context, $.fn.fumanchu.fallback, args ] );
+				out = val.apply( t, [ $.fumanchu.context, $.fumanchu.fallback, args ] );
 			// boolean
 			} else if ( $.type( val ) === 'boolean' ) {
 				out = val ? '1' : '0';
 			// number
 			} else if ( $.type( val ) === 'number' ) {
-				out = $.fn.fumanchu.numberformat( val, $.fn.fumanchu.fallback );
+				out = $.fumanchu.numberformat( val, $.fumanchu.fallback );
 			// string
 			} else if ( $.type( val ) === 'string' ) {
 				out = val;
 			}
 
-			return t.fumanchu( out, $.fn.fumanchu.context, $.fn.fumanchu.fallback, $.fn.fumanchu.args );
+			return t.fumanchu( out, $.fumanchu.context, $.fumanchu.fallback, $.fumanchu.args );
 		} );
 	};
 
 	// searches an object and returns any found path
-	$.fn.fumanchu.getpath = function getpath( path, object, fallback ) {
+	$.fumanchu.getpath = function getpath( path, object, fallback ) {
 		path = path.split( '.' );
 		if ( path.length == 4 && object && object[ path[0] ] && object[ path[0] ][ path[1] ] && object[ path[0] ][ path[1] ][ path[2] ] && object[ path[0] ][ path[1] ][ path[2] ][ path[3] ] )
 			return object[ path[0] ][ path[1] ][ path[2] ][ path[3] ];
@@ -103,18 +103,21 @@
 	};
 
 	// template cache
-	$.fn.fumanchu.templates = {};
+	$.fumanchu.templates = {};
 
 	// number handling function
-	$.fn.fumanchu.numberformat = function( num, fallback ) {
+	$.fumanchu.numberformat = function( num, fallback ) {
 		return num;
 	};
 
 	// prime template cache
 	$( document ).ready( function() {
 		$( '[data-template]' ).each( function() {
-			$.fn.fumanchu.templates[ $( this ).data( 'template' ) ] = $( this ).html();
+			$.fumanchu.templates[ $( this ).data( 'template' ) ] = $( this ).html();
 		} );
 	} );
+
+	// allow collection usage, to alter 'this' in callbacks
+	$.fn.fumanchu = $.fumanchu;
 
 })(jQuery);
